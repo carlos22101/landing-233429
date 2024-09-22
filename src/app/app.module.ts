71cb95c4a8b7dbc402dda4cdbcdd1d3ed2a7551a
carlos22101 <233429@ids.upchiapas.edu.mx>
2024-09-22 02:49:22 -0600

    cascaron y funcionalidad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MainImageComponent } from './main-image/main-image.component';
+import { DescriptionsComponent } from './descriptions/descriptions.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainImageComponent,
+    DescriptionsComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/descriptions/descriptions.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-descriptions',
+  templateUrl: './descriptions.component.html',
+  styleUrl: './descriptions.component.css'
+})
+export class DescriptionsComponent {
+
+}
