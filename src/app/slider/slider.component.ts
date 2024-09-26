3cf4059f1a1d1b420b91f44f02985785c6125586
carlos22101 <233429@ids.upchiapas.edu.mx>
2024-09-25 18:47:04 -0600

    Deploy Angular project to GitHub Pages

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -10,11 +10,11 @@ export class SliderComponent {
   slides = [
     {
       image: 'Smitejustnf.png',
-      description: 'Mapa del modo Just para 3 jugadores o duel'
+      description: 'El modo Joust en Smite es una modalidad más rápida y menos compleja que Conquista, enfocada en enfrentamientos más directos entre equipos de tres jugadores. A diferencia de Conquista, donde el mapa es más grande y hay múltiples carriles, Joust se desarrolla en un mapa más pequeño y con solo un carril principal. Este formato promueve peleas frecuentes y una mayor acción en un entorno más controlado.'
     },
     {
-      image: 'mapasmitenf.png',
-      description: 'Descripción de la imagen 2 para el slider'
+      image: 'slashmapanf.png',
+      description: 'El modo Slash en Smite es un modo de juego híbrido que combina elementos de dos mapas populares del pasado: Siege y Clash. Lanzado en 2022, Slash ofrece una experiencia rápida, intensa y llena de acción con equipos de cinco jugadores, en un mapa que tiene dos carriles y una pequeña jungla. Este modo está diseñado para ser dinámico y emocionante, con un enfoque en peleas constantes, control de objetivos, y destrucción rápida de las estructuras enemigas.'
     }
   ];
 
